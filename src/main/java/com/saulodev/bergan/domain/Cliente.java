6a90c9065aa713d67a798c71258b8b6a825ce4fb
saulo.maker <saulosousasilva3@gmail.com>
2023-05-31 00:46:29 -0300

    Teste

--- src/main/java/com/saulodev/bergan/domain/Cliente.java
+++ src/main/java/com/saulodev/bergan/domain/Cliente.java
@@ -2,8 +2,6 @@ package com.saulodev.bergan.domain;
 
 import java.util.List;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
