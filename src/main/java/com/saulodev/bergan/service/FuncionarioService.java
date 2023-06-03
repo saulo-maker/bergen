6f08de3af10a1613e630ed6ad0d51bd0520eea5c
saulo.maker <saulosousasilva3@gmail.com>
2023-06-03 18:37:27 -0300

    Teste

--- src/main/java/com/saulodev/bergan/service/FuncionarioService.java
+++ src/main/java/com/saulodev/bergan/service/FuncionarioService.java
@@ -3,6 +3,7 @@ package com.saulodev.bergan.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,9 +13,22 @@ import com.saulodev.bergan.repository.FuncionarioRepository;
 
 @Service
 public class FuncionarioService {
-    
+
     @Autowired
     private FuncionarioRepository repository;
+
+    public Funcionario alterarFuncionario(@PathVariable Long id, @RequestBody Funcionario funcionario) {
+
+		Funcionario funcionarioUpdate = repository.findById(id).orElse(null);
+		funcionarioUpdate.setId(funcionario.getId());
+		funcionarioUpdate.setLogin(funcionario.getLogin());
+		funcionarioUpdate.setSenha(funcionario.getSenha());
+		funcionarioUpdate.setFone(funcionario.getFone());
+		funcionarioUpdate.setEmail(funcionario.getEmail());
+        funcionarioUpdate.setComissao(funcionario.getComissao());
+
+		return repository.save(funcionarioUpdate);
+	}
 
     public Funcionario buscarPorUm(@PathVariable Long id) {
         return repository.findById(id).orElse(null);
