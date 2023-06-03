783891ee8349c52cdc30b74477e6c6fc994edb1f
saulo.maker <saulosousasilva3@gmail.com>
2023-06-03 18:37:36 -0300

    Classes

--- src/main/java/com/saulodev/bergan/service/FuncionarioService.java
+++ src/main/java/com/saulodev/bergan/service/FuncionarioService.java
@@ -3,7 +3,6 @@ package com.saulodev.bergan.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
